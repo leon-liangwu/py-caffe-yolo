0e0bdb2614fadd26283d47e693cd2e1efd0cfa47
leon-liangwu <wuliang0301@hotmail.com>
2019-09-04 15:05:16 +0800

    dir arrange

--- yolo/models/tiny-yolo-v2/train_tiny.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-CAFFE_HOME="../../../caffe/"
-
-SOLVER=./solver.prototxt
-WEIGHTS=../../yolo_models/darknet_v1.9.caffemodel
-
-$CAFFE_HOME/build/tools/caffe train \
-    --solver=$SOLVER  --gpu=0
